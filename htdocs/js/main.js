904b3de2fa12541a58fbf9d3fe0ef2c7cea4ea99
kao <kao@aklamio.com>
2016-03-11 16:01:05 +0100

    Add animation number 3

--- htdocs/js/main.js
+++ htdocs/js/main.js
@@ -23,5 +23,41 @@
 	 	}
 	 }).run();
 
+	 // 3
+
+	 var wrapper3 = document.querySelector('.wrapper3'),
+	 	 icon3    = wrapper3.querySelector('.ani3'),
+	 	 timeline3 = new mojs.Timeline(),
+
+	 	 burst3 = new mojs.Burst({
+	 	 	parent: wrapper3,
+	 	 	repeat: 999,
+	 	 	duration: 1500,
+			shape : 'circle',
+			fill : [ '#FE4C4A', '#FE4C4A', '#FE4C4A', '#FE4C4A', '#FE4C4A', '#FE4C4A' ],
+			x: '26%',
+			y: '50%',
+			opacity: 0.6,
+			// childOptions: { radius: {10:0} },
+			// radius: {40:120},
+			count: 6,
+			isSwirl: true,
+			isRunLess: true,
+			easing: mojs.easing.bezier(0.1, 1, 0.3, 1)
+		}),
+	 	 tween3 = new mojs.Tween({
+	 	 	repeat: 999,
+	 	 	duration: 1500,
+	 	 	onUpdate: function(progress) {
+	 	 		var bounceProgress3 = mojs.easing.bounce.out(progress);
+	 			icon3.style.transform = 'scale3d(' + bounceProgress3 + ',' + bounceProgress3 + ',1)';
+	 	 	}
+	 	 });
+	 	 timeline3.add(burst3,tween3).start();
+
+	 	 // when clicking the button start the timeline/animation:
+		// wrapper3.addEventListener('mousedown', function() {
+		// 	timeline3.start();
+		// });
 
 })();
