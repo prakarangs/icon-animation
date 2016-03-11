3179d18220e38d1dd94c562bd977c6145fb6ab52
kao <kao@aklamio.com>
2016-03-11 13:05:24 +0100

    add wrapper icon

--- htdocs/js/main.js
+++ htdocs/js/main.js
@@ -1,25 +1,25 @@
 (function() {
 
 	// 1
-	// var iconHeart = document.querySelector('.fa-heart');
-	// new mojs.Tween({
-	// 	repeat: 1,
-	// 	delay: 500,
-	// 	duration: 1500,
-	// 	onUpdate: function(progress) {
-	// 		var bounceProgress = mojs.easing.bounce.out(progress);
-	// 		iconHeart.style.transform = 'translateY(' + 200*bounceProgress + 'px)';
-	// 	}
-	// }).run();
+	var icon1 = document.querySelector('.ani1');
+	new mojs.Tween({
+		repeat: 999,
+		delay: 500,
+		duration: 1500,
+		onUpdate: function(progress) {
+			var bounceProgress = mojs.easing.bounce.out(progress);
+			icon1.style.transform = 'translateY(' + 200*bounceProgress + 'px)';
+		}
+	}).run();
 
 	// 2
-	var  icon    = document.querySelector('.ani2');
+	var  icon2 = document.querySelector('.ani2');
 	 new mojs.Tween({
 	 	repeat: 999,
 	 	duration: 900,
 	 	onUpdate: function(progress) {
 	 		var bounceProgress = mojs.easing.bounce.out(progress);
-	 		icon.style.transform = 'scale3d(' + bounceProgress + ',' + bounceProgress + ',1)';
+	 		icon2.style.transform = 'scale3d(' + bounceProgress + ',' + bounceProgress + ',1)';
 	 	}
 	 }).run();
 
